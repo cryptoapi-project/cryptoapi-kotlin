5416d2b0090a84fe0a0c7169245b509e51bc743d
s.krupenich <sjkrypaa@gmail.com>
2020-04-22 11:36:34 +0300

    Add rates history method

--- cryptoapi/src/main/java/io/pixelplex/mobile/cryptoapi/generation/RatesApi.kt
+++ cryptoapi/src/main/java/io/pixelplex/mobile/cryptoapi/generation/RatesApi.kt
@@ -4,6 +4,7 @@ import io.pixelplex.mobile.cryptoapi.annotation.CallbackError
 import io.pixelplex.mobile.cryptoapi.annotation.CallbackSuccess
 import io.pixelplex.mobile.cryptoapi.annotation.Get
 import io.pixelplex.mobile.cryptoapi.annotation.Path
+import io.pixelplex.mobile.cryptoapi.model.common.HistoryRateModel
 import io.pixelplex.mobile.cryptoapi.model.common.RateModel
 import io.pixelplex.mobile.cryptoapi.model.exception.ApiException
 
@@ -12,6 +13,13 @@ interface RatesApi {
     fun getRates(
         @Path("coins") coins: List<String>,
         @CallbackSuccess onSuccess: (List<RateModel>) -> Unit,
+        @CallbackError onError: (ApiException) -> Unit
+    )
+
+    @Get("rates/{coins}/history")
+    fun getRatesHistory(
+        @Path("coins") coins: List<String>,
+        @CallbackSuccess onSuccess: (List<HistoryRateModel>) -> Unit,
         @CallbackError onError: (ApiException) -> Unit
     )
 }
